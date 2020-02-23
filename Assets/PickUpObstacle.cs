9de48b8f5df69d7bcad50410e3f71e0773236447
MooOfDoom <45674923+MooOfDoom@users.noreply.github.com>
2020-02-22 20:34:17 -0800

    Initial commit

---
+++ Assets/PickUpObstacle.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PickUpObstacle : MonoBehaviour
+{
+	private Camera _camera;
+	private GameObject pickedObstacle;
+	private float pickedDistance;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		_camera = GetComponent<Camera>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit))
+			{
+				GameObject hitObject = hit.transform.gameObject;
+				if (hitObject.tag == "Obstacle")
+				{
+					pickedObstacle = hitObject;
+					pickedDistance = (hit.point - transform.position).magnitude;
+				}
+			}
+		}
+		else if (Input.GetMouseButtonUp(0) && pickedObstacle != null)
+		{
+			pickedObstacle = null;
+		}
+		else if (pickedObstacle != null)
+		{
+			Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+			Vector3 targetPoint = transform.position + pickedDistance*ray.direction.normalized;
+			pickedObstacle.transform.position = targetPoint;
+		}
+	}
+}
---
+++ Assets/Stalker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Stalker : MonoBehaviour
+{
+	public Transform player;
+	public Transform overview;
+
+	private Vector3 playerViewOffset;
+	private Quaternion playerViewRotation;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		playerViewOffset = transform.position - player.transform.position;
+		playerViewRotation = transform.rotation;
+	}
+
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		if (Input.GetKey("m"))
+		{
+			transform.position = overview.position;
+			transform.rotation = overview.rotation;
+		}
+		else
+		{
+			transform.position = player.transform.position + playerViewOffset;
+			transform.rotation = playerViewRotation;
+		}
+	}
+}
